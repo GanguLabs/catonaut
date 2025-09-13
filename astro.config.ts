b740aaf1503c6d73b0a1ae8197c9ff57d55f9aaa
Anwesh Gangula <gangulaanwesh@gmail.com>
2025-09-13 17:37:17 +0530

    format document

--- astro.config.ts
+++ astro.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import svelte from "@astrojs/svelte";
-import vue from "@astrojs/vue";
-import solidJs from "@astrojs/solid-js";
+import svelte from '@astrojs/svelte';
+import vue from '@astrojs/vue';
+import solidJs from '@astrojs/solid-js';
 
-import preact from "@astrojs/preact";
+import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), svelte(), vue(), solidJs(), preact()],
   build: {
-    assets: 'app'
-  }
+    assets: 'app',
+  },
 });
