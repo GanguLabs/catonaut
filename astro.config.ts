9493379f59d866ade66b29e43095502079d8db25
Anwesh Gangula <gangulaanwesh@gmail.com>
2025-09-13 18:11:03 +0530

    scoped frameworks into separate folders

--- astro.config.ts
+++ astro.config.ts
@@ -8,7 +8,22 @@ import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), svelte(), vue(), solidJs(), preact()],
+  integrations: [
+    react({
+      // Process files within the 'src/components/react' directory with React
+      include: ['**/src/components/react/*'],
+    }),
+    solidJs({
+      // Process files within the 'src/components/solid' directory with Solid
+      include: ['**/src/components/solid/*'],
+    }),
+    preact({
+      // Process files within the 'src/components/preact' directory with Preact
+      include: ['**/src/components/preact/*'],
+    }),
+    svelte(),
+    vue(),
+  ],
   build: {
     assets: 'app',
   },
