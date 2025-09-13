e728a5cf5a6279abd77a23abef13708db51d11ec
Anwesh Gangula <gangulaanwesh@gmail.com>
2025-09-13 17:25:00 +0530

    added svelte component

---
+++ src/components/SvelteComponent.svelte
@@ -0,0 +1,9 @@
+<script>
+  let count = 0;
+  function increment() {
+    count += 1;
+  }
+</script>
+
+<p>Below Button counter is loaded from a svelte component</p>
+<button on:click={increment}>Count: {count}</button>
