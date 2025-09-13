b21beab6afa3cd43967071cb98e3140d599e64a2
Anwesh Gangula <gangulaanwesh@gmail.com>
2025-09-13 18:22:33 +0530

    Added React state

--- src/components/react/ReactComponent.tsx
+++ src/components/react/ReactComponent.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function MyReactComponent({ message }: { message: string }) {
+  const [count, setCount] = useState(0);
   return (
     <div>
-      <h1>Hello from React!</h1>
-      <p>{message}</p>
+      <h2>Hello {message} from React!</h2>
+      <button onClick={() => setCount(count + 1)}>Count {count}</button>
     </div>
   );
 }
